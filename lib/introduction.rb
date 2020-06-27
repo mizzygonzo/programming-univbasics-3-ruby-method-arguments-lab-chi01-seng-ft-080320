d22f33cbf4a1017e408ca8141c720847864215f2
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-06-27 18:23:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,15 @@
+#intro 
 def introduction(name)
 puts "hi, my name is #{name}."
 end 
 
+#intro with language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
 
+
+#intro with language option 
 
 
 
